8b045d0f0205f7926a1795b4b962fae4f4e7c963
Sean <sean.mowrey9@gmail.com>
2018-03-27 20:11:10 -0400

    Update reload arms methods
    Successful gets launcher working

--- GDG_Serial.ino
+++ GDG_Serial.ino
@@ -20,19 +20,53 @@ void setup ()
 }
 
 void waitForButton() {
+  //Waits For Button Push
   while(digitalRead(buttonpin) != 1) {};
-//  while(digitalRead(buttonpin) == 1) {};
+  Serial.println("Button Pushed");
+}
+void restArms() {
+  //Returns arms to Resting Position
+  reloadRight.write(170);  
+  reloadLeft.write(0);
+  Serial.println("Arms Rested");
+}
+void reloadArms() {
+  //Push arms back with rubberband to reload loaded position
+  reloadRight.write(0); 
+  reloadLeft.write(200); 
+}
+void loadTrigger() {
+  //Puts trigger in veticle position to take load of Rubber band off reload arms
+  trigger.write(0);
+  Serial.println("Trigger Loaded");
+}
+void fireTrigger() {
+  //Return Trigger to rest Position to let Rubber Band Go
+  trigger.write(90`);
+  Serial.println("Trigger Rest Position");
 }
 
 void loop ()
-{
-  // digital interface will be assigned a value of 3 to read val
-//  waitForButton();
-  Serial.println("fuk");
-//  reloadRight.write(120); // 
-//  reloadLeft.write(150); // 0 is bottom 180
-//  delay(3000);
-  trigger.write(0); // 0 is loaded, 90 is fired
+{   
+    //Returns Arms to resting Position
+    restArms();
+    //Waits for Touch Sensor
+    waitForButton();
+    fireTrigger();
+    reloadArms();
+    //wait 3 seconds for Arms to go back with Rubber Band
+    delay(3000);
+    loadTrigger();
+    //Wait One Second for Trigger to Take load
+    delay(1000);
+    restArms();
+    //Wait 3 seconds for arms to clear firing range and go to rest
+    delay(3000);
+    //FIRE EASTER EGG
+    fireTrigger();
+    Serial.println("Fire!");
+    delay(3000);
+
 
 
 }
